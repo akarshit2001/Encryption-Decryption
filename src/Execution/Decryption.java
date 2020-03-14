f97b104bec160087838eb718fc78cc9f28ed24ce
akarshit <akarshitsrivastava33@gmail.com>
2020-03-14 15:16:56 +0530

    decryption method is completed.

--- src/Execution/Decryption.java
+++ src/Execution/Decryption.java
@@ -1,4 +1,29 @@
 package Execution;
 
 public class Decryption {
+
+
+    public String decryption(String encrypt, int shift) {
+        String encryption = "";
+        for (int i = 0; i < encrypt.length(); i++) {
+            if (Character.isUpperCase(encrypt.charAt(i))) {
+                char character = encrypt.charAt(i);
+                int b = (int) character;
+                int n = ((b + shift - 65) % 26 + 65);
+
+                encryption = encryption + Character.toString((char) n);
+
+                // System.out.println(encryption);
+            } else if (Character.isLowerCase(encrypt.charAt(i))) {
+                char character = encrypt.charAt(i);
+                int b = (int) character;
+                int n = ((b + shift - 97) % 26 + 97);
+
+                encryption = encryption + Character.toString((char) n);
+
+
+            }
+        }
+        return encryption;
+    }
 }
