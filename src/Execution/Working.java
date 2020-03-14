1a08eb5e2e3d209e581bb53bcbf9c7e0a252af72
akarshit <akarshitsrivastava33@gmail.com>
2020-03-14 14:38:04 +0530

    encryption method is done

--- src/Execution/Working.java
+++ src/Execution/Working.java
@@ -2,6 +2,26 @@ package Execution;
 
 public class Working {
     public void encryption(String input, int shift) {
+        String encryption = "";
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isUpperCase(input.charAt(i))) {
+                char character = input.charAt(i);
+                int b = (int) character;
+                int n = ((b + shift - 65) % 26 + 64);
 
+                encryption = encryption + Character.toString((char) n);
+
+                // System.out.println(encryption);
+            } else if (Character.isLowerCase(input.charAt(i))) {
+                char character = input.charAt(i);
+                int b = (int) character;
+                int n = ((b + shift - 97) % 26 + 94);
+
+                encryption = encryption + Character.toString((char) n);
+
+
+            }
+        }
+        System.out.println(encryption);
     }
 }
