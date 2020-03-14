2f8bc9486c88a35379a0f9207dc0e5cc69a6f59b
akarshit <akarshitsrivastava33@gmail.com>
2020-03-14 12:57:27 +0530

    initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+import Execution.Working;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Working wc = new Working();
+        System.out.println("Enter the String");
+        String input = sc.next();
+        System.out.println("Enter the no. that you want to shift ");
+        sc.nextLine();
+        int n = sc.nextInt();
+
+    }
+
+}
