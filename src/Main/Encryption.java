3f93c89144b9cfb8b08ba681be5dd03fe840da55
akarshit <akarshitsrivastava33@gmail.com>
2020-04-01 23:33:09 +0530

    Commandlinestarting begin

--- src/Main/Encryption.java
+++ src/Main/Encryption.java
@@ -3,7 +3,7 @@ package Main;
 public class Encryption {
     public String encryption(String input, int shift) {
 
-
+        System.out.println(input);
         String s = "";
         for (int i = 0; i < input.length(); i++) {
             char ch = (char) ((int) (input.charAt(i)) + shift);
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,39 +1,33 @@
 package Main;
 
 
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Decryption decryption = new Decryption();
         Encryption encryption = new Encryption();
         int count = 2;
-        while (count != 0) {
-            System.out.println("Enter the mode");
-            String mode = sc.next();
-            sc.nextLine();
-            if (mode.equals("enc")) {
-                System.out.println("Enter the string thar you want to encrypt");
-                String input = sc.nextLine();
-
-                System.out.println("Enter the key");
-                int key = sc.nextInt();
-                String encrypt = encryption.encryption(input, key);
-                System.out.println(encrypt);
-            } else if (mode.equals("dec")) {
-                System.out.println("Enter the string thar you want to decrypt ");
-                String input = sc.nextLine();
-
-                System.out.println("Enter the key");
-                int key = sc.nextInt();
-                String decryption1 = decryption.decryption(input, key);
-                System.out.println(decryption1);
-
-            }
-            count--;
+        String mode;
+        if (args[0].equals("")) {
+            mode = "enc";
+        } else {
+            mode = args[0];
         }
+        System.out.println(mode);
+        if (mode.equals("enc")) {
+            String input = args[1];
+            String key = args[2];
+            int finakey = Integer.parseInt(key);
+            String encrypt = encryption.encryption(input, finakey);
+            System.out.println(encrypt);
+        } else if (mode.equals("dec")) {
+            String input = args[1];
+            int key = Integer.parseInt(args[2]);
+            String decryption1 = decryption.decryption(input, key);
+            System.out.println(decryption1);
+
+        }
+
     }
 
 }
+
