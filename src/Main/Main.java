aab02c5a2d0026bf8645863590ec870e9bb7f65d
akarshit <akarshitsrivastava33@gmail.com>
2020-04-01 12:42:16 +0530

    Main class is modified.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,26 +1,39 @@
 package Main;
 
-import Execution.Decryption;
-import Execution.Encryption;
+
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Encryption wc = new Encryption();
-        Decryption dc = new Decryption();
-        System.out.println("Enter the String");
-        String input = sc.nextLine();
-        System.out.println("Enter the no. that you want to shift ");
-        System.out.println(input);
-        int n = sc.nextInt();
-        System.out.println("Encryption :-");
-        String encrypt = wc.encryption(input, n);
-        System.out.println(encrypt);
-        System.out.println("Decryption :-");
-        String decrypt = dc.decryption(encrypt, n);
-        System.out.println(decrypt);
+        Decryption decryption = new Decryption();
+        Encryption encryption = new Encryption();
+        int count = 2;
+        while (count != 0) {
+            System.out.println("Enter the mode");
+            String mode = sc.next();
+            sc.nextLine();
+            if (mode.equals("enc")) {
+                System.out.println("Enter the string thar you want to encrypt");
+                String input = sc.nextLine();
+
+                System.out.println("Enter the key");
+                int key = sc.nextInt();
+                String encrypt = encryption.encryption(input, key);
+                System.out.println(encrypt);
+            } else if (mode.equals("dec")) {
+                System.out.println("Enter the string thar you want to decrypt ");
+                String input = sc.nextLine();
+
+                System.out.println("Enter the key");
+                int key = sc.nextInt();
+                String decryption1 = decryption.decryption(input, key);
+                System.out.println(decryption1);
+
+            }
+            count--;
+        }
     }
 
 }
