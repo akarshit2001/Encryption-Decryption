9f058ada7172b2580825ddbc0c88b0e6f1325da2
admin@atul <atulsingh29@protonmail.com>
2020-04-01 21:28:06 +0530

    cmd enabled

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,39 +1,60 @@
 package Main;
 
 
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int testIndexMode=index(args,"-mode");
+        int testIndexData=index(args,"-data");
+        int testIndexKey=index(args,"-key");
+        String finalMode;
+        try{
+            finalMode=args[testIndexMode+1];}
+        catch (NullPointerException e){
+            finalMode ="enc";
+        }
+
+        String finalData=args[testIndexData+1];
+
+        int finalKey;
+        try {
+            finalKey = Integer.parseInt(args[testIndexKey + 1]);
+        }
+        catch (NullPointerException e){
+            finalKey=0;
+        }
+
+
         Decryption decryption = new Decryption();
         Encryption encryption = new Encryption();
-        int count = 2;
-        while (count != 0) {
-            System.out.println("Enter the mode");
-            String mode = sc.next();
-            sc.nextLine();
-            if (mode.equals("enc")) {
-                System.out.println("Enter the string thar you want to encrypt");
-                String input = sc.nextLine();
+        String mode = finalMode;
+        if (mode.equals("enc")) {
+            //System.out.println("Enter the string thar you want to encrypt");
+            String input = finalData;
 
-                System.out.println("Enter the key");
-                int key = sc.nextInt();
-                String encrypt = encryption.encryption(input, key);
-                System.out.println(encrypt);
-            } else if (mode.equals("dec")) {
-                System.out.println("Enter the string thar you want to decrypt ");
-                String input = sc.nextLine();
+            //System.out.println("Enter the key");
+            String encrypt = encryption.encryption(input, finalKey);
+            System.out.println("/"+encrypt);
+        } else if (mode.equals("dec")) {
+            //System.out.println("Enter the string thar you want to decrypt ");
+            String input = finalData;
 
-                System.out.println("Enter the key");
-                int key = sc.nextInt();
-                String decryption1 = decryption.decryption(input, key);
-                System.out.println(decryption1);
+            //System.out.println("Enter the key");
+            String decryption1 = decryption.decryption(input, finalKey);
+            System.out.println("/"+decryption1);
 
-            }
-            count--;
         }
+
+    }
+
+    static int index(String[] arr,String searchTerm){
+        int response=-1;
+        int count=0;
+        for (String test:
+                arr) {
+            if(test.equals(searchTerm)){response=count;}
+            else {count++;}
+        }
+        return response;
     }
 
 }
