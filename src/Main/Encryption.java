6c9d3ce0b9d9d58c6773f1f439d1fba9b4f36651
akarshit <akarshitsrivastava33@gmail.com>
2020-04-08 22:53:26 +0530

    Encryption.java is done.

--- src/Main/Encryption.java
+++ src/Main/Encryption.java
@@ -1,16 +1,23 @@
 package Main;
 
 public class Encryption {
+    /**
+     * @param input is the data given by user to encrypt .
+     * @param shift is the key which tells by how much each letter to be shifted.
+     * @return the string which is now encrypted.
+     */
     public String encryption(String input, int shift) {
 
-        System.out.println(input);
-        String s = "";
+
+        String string = "";
+
+        // this loop is used to traverse each character of string and increment it's ascii value by their shift key.
         for (int i = 0; i < input.length(); i++) {
             char ch = (char) ((int) (input.charAt(i)) + shift);
-            s = s + Character.toString(ch);
+            string = string + Character.toString(ch);
         }
 
 
-        return s;
+        return string;
     }
 }
