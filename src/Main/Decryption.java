449c204c2d3eb57ea8ff2348e8d356a5f210df54
akarshit <akarshitsrivastava33@gmail.com>
2020-04-08 22:52:51 +0530

    Decryption.java is done.

--- src/Main/Decryption.java
+++ src/Main/Decryption.java
@@ -1,14 +1,19 @@
 package Main;
 
 public class Decryption {
-
+    /**
+     * @param encrypt string pass from user .
+     * @param shift   integer by which each letter will be shifted.
+     * @return string which is decrypt.
+     */
 
     public String decryption(String encrypt, int shift) {
         String decryption = "";
+        // this loop is used to traverse each character of string and decrement it's ascii value by their shift key.
 
         for (int i = 0; i < encrypt.length(); i++) {
-            char ch = (char) ((int) (encrypt.charAt(i)) - shift);
-            decryption = decryption + Character.toString(ch);
+            char character = (char) ((int) (encrypt.charAt(i)) - shift);
+            decryption = decryption + Character.toString(character);
 
         }
         return decryption;
